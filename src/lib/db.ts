33f6cad66827dbeaead2f6fb7eb3bba4a596e63e
mvpcyber <mvpcyberjakarta@gmail.com>
2026-05-13 17:46:40 +0700

    data

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,14 +1,15 @@
 import mysql from 'mysql2/promise';
 
 const pool = (globalThis as any).pool || mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '', // Default XAMPP password is empty
-  database: 'kpsdata',
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'kpsdata',
+  password: process.env.DB_PASSWORD || 'Bangbens220488!',
+  database: process.env.DB_NAME || 'kpsdata',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
+
 
 if (process.env.NODE_ENV !== 'production') {
   (globalThis as any).pool = pool;
